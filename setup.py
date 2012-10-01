002cc9b70b7bbf8e65ef8b9845b48f9bcb2ea626
Mark Harrison <mark@mivok.net>
2012-10-01 17:05:12 -0400

    Updated setup.py and add a license file

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup (name="circonusapi",
-       version="0.1.0",
-       description="Circonus API library",
-       author="Mark Harrison",
-       author_email="mark@omniti.com",
-       url="http://github.com/omniti-labs/circonusapi",
-       license="ISC",
-       packages=['circonusapi'],
+setup (
+    name="circonusapi",
+    version="0.1.0",
+    description="Circonus API library",
+    author="Mark Harrison",
+    author_email="mark@omniti.com",
+    url="http://github.com/omniti-labs/circonusapi",
+    license="ISC",
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Development Status :: 4 - Beta",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Monitoring"
+    ],
+    packages=['circonusapi']
 )
