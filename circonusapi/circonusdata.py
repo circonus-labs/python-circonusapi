0dd1aea44eb98d69d1022db24f2d5785a6e55ed8
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2020-01-16 18:01:04 +0100

    CirconusData.caql(): output results as DF4

--- circonusapi/circonusdata.py
+++ circonusapi/circonusdata.py
@@ -7,35 +7,19 @@ To gain access to the full functionality use the circonusapi module.
 
 import math
 from datetime import datetime
-
-# At the time of this writing circllhist is not available on pip.
-# You can get it from: github.com/circonus-labs/libcircllhist/
-from circllhist import Circllhist
+import warnings
 
 from . import circonusapi
 
-################################################################################
-## Helper Functions
+#
+# Optional Imports
+#
 
-def _caql_infer_type(res):
-    if len(res[0]) >= 3 and isinstance(res[0][2], dict):
-        return "histogram"
-    else:
-        return "numeric"
+try:
+    from circllhist import Circllhist
+except ImportError:
+    Circllhsit = None
 
-def _fix_time(start, period):
-    if isinstance(start, datetime):
-        return _fix_time(start.timestamp(), period)
-    if not start % period == 0:
-        raise Exception(
-            "start parameter {} is not divisible by period {}. Use e.g. {} instead.".format(
-                start, period, math.floor(start / period)
-            )
-        )
-    return start
-
-################################################################################
-## Classes
 
 class CirconusData(object):
     "Circonus data fetching class"
@@ -46,32 +30,55 @@ class CirconusData(object):
     def caql(self, query, start, period, count):
         """
         Fetch data using CAQL.
-        Returns a map: slot_name => list
-        Limitations:
-        - slots_names are currently output[i]
-        - For histogram output only a single slot is returned
+
+        Args:
+           query (str): the CAQL query string
+           start (int/dateimte): starttime of the query. Either UNIX timestamp in seconds 
+                 or datetime object
+           period (int): period of data to fetch
+           count (int): number of datapoints to fetch
+
+        Returns:
+           res (dict): result in DF4 format
+           {
+             "head" : { count = 60, start = ..., period = 60 }
+             "meta" : [ {kind:"numeric", label:"some_metric"}, {...}, ... ] -- per metric metadata
+             "data" : [ [1,1,1,1,...], [2,2,2,2,...] ] -- per metric data
+           }
         """
-        start = _fix_time(start, period)
+        if isinstance(start, datetime):
+            start = start.timestamp()
+        if not start % period == 0:
+            new_start = math.floor(start / period)
+            warnings.warn(
+                "start parameter {} is not divisible by period {}. Using {} instead.".format(
+                    start, period, new_start))
+            start = new_start
+
         params = {
             "query": query,
             "period": period,
             "start": int(start),
             "end": int(start + count * period),
+            "format" : "DF4"
         }
-        res = self._api.api_call("GET", "/caql", params=params)["_data"]
-        if _caql_infer_type(res) == "histogram":
-            # In this case, we have only a single output metric and res looks like:
-            # res = [[1467892920, 60, {'1.2e+02': 1, '2': 1, '1': 1}], ... ]
-            return {
-                "time": [row[0] for row in res],
-                "output[0]": [Circllhist.from_dict(row[2]) for row in res],
-            }
-        else:
-            # In the numeric case res looks like this:
-            # res = [[1467892920, [1, 2, 3]], [1467892980, [1, 2, 3]], ... ]
-            out = {}
-            width = len(res[0][1])
-            out["time"] = [row[0] for row in res]
-            for i in range(width):
-                out["output[{}]".format(i)] = [row[1][i] for row in res]
-            return out
+        res = self._api.api_call("GET", "/caql", params=params)
+
+        #
+        # Convert histogram JSON values to Circllhist objects.
+        #
+        # Don't convert histogram output if Circllhist was not found on the system
+        if not Circllhist:
+            return res
+
+        # In the case of 0 output metrics, res['meta'] itself might be None
+        if not res['meta']:
+            return res
+
+        assert(len(res['meta']) == len(res['data']))
+        width = len(res['meta'])
+        for i in range(width):
+            if res['meta'][i]['kind'] == "histogram":
+                res['data'][i] = [ Circllhist.from_dict(h) for h in res['data'][i] ]
+
+        return res
